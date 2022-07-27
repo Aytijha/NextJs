03656a8a4b7cf3bb949c12fd5fbb3b1ac679f560
Aytijha Chakraborty <aytijhachakraborty.cse19011511@gehu.ac.in>
2022-07-27 21:03:02 +0530

    file based routing

---
+++ 2-file-based-routing/pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>This is my About page. How are you?</div>
+  )
+}
+
+export default about
---
+++ 2-file-based-routing/pages/blogpost/how-to-use-keyboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const how = () => {
+  return (
+    <div>How to use a Keyboard?</div>
+  )
+}
+
+export default how
